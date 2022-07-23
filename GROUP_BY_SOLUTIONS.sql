f95989b2c266ce7e4d51b245144e7e3e8aa53f86
Mahmud Oyinloye <64417495+le-oasis@users.noreply.github.com>
2022-07-23 18:47:48 +0100

    GROUP_BY QA

---
+++ GROUP_BY_SOLUTIONS.sql
@@ -0,0 +1,67 @@
+-- Welcome to the Oasis --
+-- This lecture contains challenge tasks for the group by section of the course.
+---------------------------------------------------------------------------
+-- You have been given a set of Challenge Queries (CQ) to answer below.
+---------------------------------------------------------------------------
+
+---------------------------------------------------------------------------
+-- CQ 1
+-- We have two staff members with staff IDs one and two.
+-- We want to give a bonus to the staff member that handled the most payments, most in terms of number
+-- of payments processed, not total dollar amount.
+-- So how many payments did each staff member handle and who gets the bonus?
+---------------------------------------------------------------------------
+-- Hints:
+-- Use the payment table,
+-- Understand the difference between COUNT and SUM.
+---------------------------------------------------------------------------
+SELECT staff_id, COUNT(amount)
+FROM payment
+GROUP BY staff_id;
+---------------------------------------------------------------------------
+
+---------------------------------------------------------------------------
+-- CQ 2
+-- Corporate headquarters is conducting a study on the relationship
+-- between replacement cost and the movie's MPAA rating.
+-- That is to say its rating of G, PG, are, etc..
+-- What is the average replacement cost per MPAA rating?
+-- Note You may need to expand the average column to view the correct results
+---------------------------------------------------------------------------
+-- Hints:
+-- The hint is to use the film table
+-- and to also recall the average returns back many significant digits.
+-- So you can either stretch out the column as you just saw, or you can use the round function to fix
+-- this issue.
+---------------------------------------------------------------------------
+SELECT rating, ROUND(AVG(replacement_cost),2) AS avg_rep_cost
+FROM film
+GROUP BY rating;
+---------------------------------------------------------------------------
+
+---------------------------------------------------------------------------
+-- CQ 3
+-- We're running a promotion to reward our top five customers with coupons.
+-- What are the customer IDs of the top five customers based off total expenditure or total spend?
+-- That is the actual dollar amount
+---------------------------------------------------------------------------
+-- Hints:
+-- Use the payment table
+-- Use order by and
+-- Recall, you can order by the results of an aggregate function and
+-- You may want to use limit to view just the top five results.
+---------------------------------------------------------------------------
+SELECT customer_id, SUM(amount)
+FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC
+LIMIT 5;
+---------------------------------------------------------------------------
+-- TECHNICAL ANSWER
+-- customer ids of the top 5 customers based by total spend?
+SELECT customer_id, ROUND(SUM(amount), 2)
+FROM payment
+GROUP BY customer_id
+ORDER BY 2 DESC
+LIMIT 5;
+---------------------------------------------------------------------------
